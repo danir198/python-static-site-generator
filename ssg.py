8e19e4154c7e4b5c02548e3984bcd5cc62198130
dani rosyadi <dani198@gmail.com>
2020-08-29 22:21:25 +0700

    fix typo in ssg.py

--- ssg.py
+++ ssg.py
@@ -1,10 +1,10 @@
 import typer
+from ssg.site import Site
 
-from ssg.site import Site 
 
-def main(source="content", dest="dest"):
-	config = {"source": source, "dest": dest}
+def main(source="content", dest="dist"):
+    config = {"source": source, "dest": dest}
+    Site(**config).build()
 
-	Site(**config).build()
 
-typer.run(main)	
+typer.run(main)
